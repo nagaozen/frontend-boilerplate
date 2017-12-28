4e5f54a04bbd4b3fe630f555068cf7a866049a6b
Fabio Zendhi Nagao <nagaozen@gmail.com>
2017-12-28 12:24:20 -0200

    better landing page

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -11,7 +11,13 @@ var App = observer( React.createClass({
 	<h2>{store.subtitle}</h2>
 	<h3>made with ❤ by {store.author}</h3>
 	<p>
-Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vel rhoncus augue. Aliquam sed convallis nisl. Suspendisse a odio blandit, convallis nibh vitae, malesuada mi. Nulla facilisi. Duis felis eros, hendrerit vel gravida sed, consequat sit amet dui. Donec molestie, dolor eget porttitor molestie, ante mauris lacinia lacus, ut rhoncus est turpis a nisl. In condimentum sem eu lorem vulputate, nec ultrices est tempor. Maecenas quis faucibus odio. Cras dui libero, sollicitudin id diam a, euismod ultrices nibh.
+		What's so great about this? Well, it's a <strong>Production ready</strong> boilerplate:
+		<ul>
+			<li>Optimised and Minified <a href="https://www.w3.org/TR/CSS/"><abbr title="Cascading Style Sheets">CSS</abbr></a>, based on <a href="https://stylus-lang.com/">Stylus</a> with <abbr title="Hot Module Reload">HMR</abbr>. <em>i.e. make designers life awesome.</em></li>
+			<li>Component-based, cacheable <a href="https://reactjs.org/">React</a>+<a href="https://reactjs.org/docs/introducing-jsx.html">JSX</a> and <a href="https://mobx.js.org/">MobX</a> software engineering targeted. <em>i.e. make front-end developers life awesome.</em></li>
+			<li><code>ES6</code> goodies and examples without <code>classes</code>, because <strong>composition is better than inheritance</strong>, but you can use 'em if you want.</li>
+			<li>No <code>ES.next @decorators</code> examples, because there are a huge amount of examples with it, but knowing how to live without it is important too.</li>
+		</ul>
 	</p>
 </main>
 		);
