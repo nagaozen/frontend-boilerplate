4d9dcaa8b4751590e03ccaa81a341093cb786580
Fabio Zendhi Nagao <nagaozen@gmail.com>
2018-03-31 17:31:47 -0300

    support for legacy decorators, for a clean conscience

--- src/components/document.jsx
+++ src/components/document.jsx
@@ -1,13 +1,19 @@
-import React from "react";
+import React, { Component } from 'react';
+import { observer } from "mobx-react";
 
-var App = React.createClass({
 
-	render: function(){return(
+@observer
+class App extends Component {
+
+	render() {
+		return (
 <main>
 	<h1>Document</h1>
+	<p>{ this.props.store.paragraph }</p>
 </main>
-	)}
+		)
+	}
 
-});
+}
 
 export default App;
--- src/document.js
+++ src/document.js
@@ -1,7 +1,16 @@
 import React from "react";
 import { render } from "react-dom";
+import { observable } from "mobx";
 
 import App from "./components/document.jsx";
 import "./app.styl";
 
-render( <App />, document.getElementById("app") );
+var store = observable({
+
+	paragraph: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer eu enim in erat tincidunt porta sodales auctor leo. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Aliquam erat volutpat. Nunc ultrices malesuada porttitor. Suspendisse nec erat ac ligula viverra maximus. Duis laoreet libero lacus, eget mattis odio semper quis. Duis vel mauris eget purus efficitur blandit ac ut risus. Fusce laoreet lacus in ultrices luctus. Fusce malesuada ligula sed ex dapibus, sed commodo lacus pharetra. Suspendisse nibh lacus, tempor eu vehicula id, aliquet nec velit. Morbi at dui vehicula, semper nisl vitae, bibendum neque. Curabitur luctus mauris nec semper dignissim. "
+
+});
+
+render( <App store={store} />, document.getElementById("app") );
+
+window.store = store;
